eb2822eccfa96fd85d3359e5ffa078dca175e2ae
Eric D. Triton <alexandersegerev@gmail.com>
2025-07-01 11:29:49 -0400

    Updated base for netlify to operate

--- win10-crapp/vite.config.js
+++ win10-crapp/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/win10-conditional-rendering",
+  base: "/",
   plugins: [react()],
 })
